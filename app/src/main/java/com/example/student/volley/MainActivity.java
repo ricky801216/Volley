bfb3d2aa021d7c5e9235189f08ea5b5701385a3f
ricky801216 <a0923966051@gmail.com>
2016-08-03 14:49:50 +0800

    利用Gson的兩種方法來抓取String url的資料

--- app/src/main/java/com/example/student/volley/MainActivity.java
+++ app/src/main/java/com/example/student/volley/MainActivity.java
@@ -10,6 +10,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
        // String url = "https://www.google.com.tw/";
        // String url = "http://data.ntpc.gov.tw/od/data/api/04958686-1B92-4B74-889D-9F34409B272B;jsessionid=62C3922EA2AD7CC85E984C4F5C2423DA?$format=json";
 
-         String url ="http://udn.com/rssfeed/news/1"; /*聯合新聞網的XML讀取不到是因為它可能是UTF-8編碼問題才會導致讀取失敗，因此需要建立一個
+         String url ="http://data.ntpc.gov.tw/od/data/api/B1464EF0-9C7C-4A6F-ABF7-6BDF32847E68?$format=json"; /*聯合新聞網的XML讀取不到是因為它可能是UTF-8編碼問題才會導致讀取失敗，因此需要建立一個
         新的類別 UTF-8StringRequest 來進行解碼*/
        // String url ="http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b";
                 StringRequest request = new UTF8StringRequest(url
@@ -32,6 +40,30 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(String response) {
                 Log.d("NET", response);
 
+                //建立一個 Gson 物件
+                Gson gson = new Gson();
+                //陣列為GsonParking             由gson.fromJason取得JSON以及Type資源由 GsonParking 取得
+                ArrayList<GsonParking> mylist = gson.fromJson(response, new TypeToken<ArrayList<GsonParking>>() {}.getType());
+                //進行防呆機制
+                for(GsonParking p : mylist)
+                {
+                    Log.d("NET", p.NAME+","+p.ADDRESS);
+                }
+//                try {
+//                    JSONArray array = new JSONArray(response);
+//
+//                    for (int i = 0; i < array.length(); i++)
+//                    {
+//                        JSONObject obj = array.getJSONObject(i);
+//                        Log.d("NET", "Name:" + obj.getString("NAME") + " Addr:" + obj.getString("ADDRESS"));
+//
+//                    }
+//
+//
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
             }
         }, new Response.ErrorListener(){
 
