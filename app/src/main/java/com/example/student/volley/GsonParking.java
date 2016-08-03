3b6ad4871e80ae8770b8ad8ff2149873ec63a492
ricky801216 <a0923966051@gmail.com>
2016-08-03 14:51:49 +0800

    利用Gson的兩種方法來抓取String url的資料

---
+++ app/src/main/java/com/example/student/volley/GsonParking.java
@@ -0,0 +1,21 @@
+package com.example.student.volley;
+
+/**
+ * Created by Student on 2016/8/3.
+ */
+public class GsonParking {
+    String ID;
+    String AREA;
+    String TYPE;
+    String NAME;
+    String SUMMARY;
+    String ADDRESS;
+    String TEL;
+    String PAYEX;
+    String SERVICETIME;
+    String TW97X;
+    String TW97Y;
+    String TOTALCAR;
+    String TOTALMOTOR;
+    String TOTALBIKE;
+}
